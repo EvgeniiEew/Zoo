c626ac483929c6cf3786c28d08d021617f749bdf
Evgeniy Slabysh <evgenii1900@gmail.com>
2020-07-20 23:27:28 +0300

    strich straus = gson.fromJson

--- src/main/java/by/home/zoo/Runner.java
+++ src/main/java/by/home/zoo/Runner.java
@@ -5,7 +5,12 @@ import by.home.zoo.impl.animals.Ostrich;
 import by.home.zoo.impl.animals.Shark;
 import by.home.zoo.service.DailyService;
 import by.home.zoo.service.DayScheduler;
+import com.google.gson.Gson;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Optional;
 import java.util.Timer;
 
 public class Runner {
@@ -16,17 +21,29 @@ public class Runner {
 
         zoo.addCell(3, 5);
 
-        Ostrich straus = new Ostrich(false, "Стредний", "коричневый", "Антон", 80, "быстрый", 5, 10);
+        //  Ostrich straus = new Ostrich(false, "Стредний", "коричневый", "Антон", 80, "быстрый", 5, 10);
+        String fileName = "C:\\Users\\Jack\\Desktop\\Zoo\\src\\main\\resources\\animals.json";
+        try {
+            String line = Files.lines(Paths.get(fileName)).reduce("", String::concat);
+            Gson gson = new Gson();
+            Ostrich straus = gson.fromJson(line, Ostrich.class);
+            zoo.addAnimal(straus);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
         Shark akula = new Shark("Средний", "Серая", "Вася", 120, 10, 2);
         Crocodile crocodile = new Crocodile(123, "Large", "Brown", "Avon", 180, 5, 30);
 
-        zoo.addAnimalSpecialist(30,"vasa",500,30);
-        zoo.addBoogalter(500,40,"valera",20);
+        zoo.addAnimalSpecialist(30, "vasa", 500, 30);
+        zoo.addBoogalter(500, 40, "valera", 20);
         zoo.addCleaner("Vasiiy", 36, 500, 10);
         zoo.addCleaner("Vladimir", 36, 500, 20);
         zoo.addCleaner("Nikolay", 24, 500, 0);
 
-        zoo.addAnimal(straus);
+
         zoo.addAnimal(akula);
         zoo.addAnimal(crocodile);
 
